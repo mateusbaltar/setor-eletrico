8f875c8fa8e6bd064e8d90a6cd07a2032db0d892
Mateus Mendonca <mateusbaltar@gmail.com>
2020-03-14 10:45:29 -0300

    commit on 'master'

--- AcessoSINtegre.py
+++ AcessoSINtegre.py
@@ -2,33 +2,42 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
-CHROME_PATH = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'
-CHROMEDRIVER_PATH = 'C:/Users/Mateus Mendonca/Downloads/chromedriver.exe'
-WINDOW_SIZE = "1920,1080"
+def download_ipdo(driver):
+    driver.get('ipdo_path')
 
-options = Options()
-options.add_argument("--window-size=%s" % WINDOW_SIZE)
-options.binary_location = CHROME_PATH
+def chrome_driver():
+    CHROME_PATH = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'
+    CHROMEDRIVER_PATH = 'C:/Users/Mateus Mendonca/Downloads/chromedriver.exe'
+    WINDOW_SIZE = "1920,1080"
 
-driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=options)  
+    options = Options()
+    options.add_argument("--window-size=%s" % WINDOW_SIZE)
+    options.binary_location = CHROME_PATH
 
-# driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=options)  
+
+    # driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
 
 
-# options = webdriver.ChromeOptions()
-# driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
-# driver = webdriver.Chrome(executable_path='C:/Users/mateus.mendonca/Downloads/chromedriver.exe', chrome_options=options)
+    # options = webdriver.ChromeOptions()
+    # driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+    # driver = webdriver.Chrome(executable_path='C:/Users/mateus.mendonca/Downloads/chromedriver.exe', chrome_options=options)
 
-driver.get('https://sintegre.ons.org.br/')
-assert 'ONS' in driver.title
+    driver.get('https://sintegre.ons.org.br/')
+    assert 'ONS' in driver.title
 
-elem = driver.find_element_by_name('username')
-elem.send_keys('mateus.mendonca')
-elem = driver.find_element_by_name('password')
-elem.send_keys('Energia2019')
-login_attempt = driver.find_element_by_xpath("//*[@type='submit']")
-login_attempt.click()
+    elem = driver.find_element_by_name('username')
+    elem.send_keys('mateus.mendonca')
+    elem = driver.find_element_by_name('password')
+    elem.send_keys('Energia2019')
+    login_attempt = driver.find_element_by_xpath("//*[@type='submit']")
+    login_attempt.click()
 
-input('Aguarde...')
+    download_ipdo(driver)
 
-driver.quit()
+    input('Aguarde...')
+
+    driver.quit()
+
+
+chrome_driver()
